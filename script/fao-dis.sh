6a2e8a33ac4e0eb843dea5d5d83ff71851341190
Trond Trosterud <trond.trosterud@uit.no>
2009-11-05 10:48:53 +0000

    scripts

---
+++ script/fao-dis.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# fao-snt.sh
+# This is a shell script for analysing Northern Sámi sentences.
+# It gives the analysis, and the number of the rules used to disambiguate
+
+if [ `hostname` == 'victorio.uit.no' ]
+then
+    LOOKUP=/opt/sami/xerox/c-fsm/ix86-linux2.6-gcc3.4/bin/lookup
+else
+    LOOKUP=`which lookup`
+fi
+
+while [ 1 ]                                 # as long as there is input
+do                                          # run the following loop
+echo -n "Skriv setning: "                   # (message to user)
+read sentence                               # next 3 lines is the usual command
+echo $sentence | preprocess --abbr=~/gtsvn/st/fao/bin/abbr.txt | \
+$LOOKUP -flags mbTT -utf8 ~/gtsvn/st/fao/bin/fao.fst | ~/gtsvn/st/script/lookup2cg | \
+ vislcg3 -g ~/gtsvn/st/fao/src/fao-dis.rle --trace  # no m4
+#dis.sh --grammar ~/st/fao/bin/fao-dis.rle --minimal  # m4
+
+done                      
+exit 0

bcd025dd5f27c95a45072c837a35715561b6b404
Flammie A Pirinen <flammie@iki.fi>
2011-10-28 09:38:19 +0000

    actually test link with -L

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@ if test -z $GTCORE ; then
     echo variable GTCORE not set, run gtsetup.sh and retry
     exit 1
 fi
-if ! test -f am-shared ; then
+if ! test -L am-shared ; then
     ln -s $GTCORE/am-shared am-shared
 fi
 autoreconf -i

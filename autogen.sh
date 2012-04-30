ff0e1a1c363573fae4c5044ee57331a8b2c94a9c
Sjur N Moshagen <sjurnm@mac.com>
2012-04-30 21:21:02 +0000

    Added comments copied from tlh.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+
+# The whole GT infra requires GTCORE to be accessible at setup time
 if test -z $GTCORE ; then
     echo variable GTCORE not set, run gtsetup.sh and retry
     exit 1
 fi
+
+# the include directive of automake needs to have shared templates
+# in $topdir at setup time; currently we use symlinks to achieve this
 if ! test -L am-shared ; then
     ln -s $GTCORE/am-shared am-shared
 fi
+
+# autoreconf should work for most platforms
 autoreconf -i

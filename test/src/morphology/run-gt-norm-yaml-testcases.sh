3c3fdaf4540fd18cefcc2a8fb606c51affd43dd3
Sjur Moshagen <sjurnm@mac.com>
2012-12-18 14:12:10 +0000

    Refactored the yaml test code, moving duplicate parts to a separate file. Makes for much easier adaption to new transducer types, as well as much simplified maintenance. The svn merge test from sma turned out well - renaming files ahead of the merge seems to make the merge go smoother.
    Update from template.
    
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fao@67017 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ test/src/morphology/run-gt-norm-yaml-testcases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a shell script that will call the actual test runner with the
+# specified transducer. This determines also the set of yaml test files looped
+# over by the test runner.
+
+###### Variables: #######
+transducer=gt-norm
+
+source ./run-yaml-testcases.sh $transducer

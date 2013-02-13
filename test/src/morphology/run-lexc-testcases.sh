98d76ad07403c58fa9f55c1a0a08b73a6e92d279
Sjur N Moshagen <sjurnm@mac.com>
2013-02-13 18:24:04 +0000

    Major update to the LexC testing. Now test data directly in the LexC code is supported by the python test script morph-tester.py (it reads the lexc files directly), which solves the bugs with multiple wordforms for the same morphosyntactic inflection. It is also a bit faster than the awk solution, and allows an unlimited number of different transducers to be tested dynamically directly in the lexc code.
    NB! One file has been renamed, which will cause issues when updating. An e-mail will be sent out after this template merge - follow the instructions given there to avoid conflicts.

---
+++ test/src/morphology/run-lexc-testcases.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This is a shell script that will call the actual test runner with the
+# specified transducer. This determines also the set of yaml test files looped
+# over by the test runner.
+
+###### Variables: #######
+transducer=gt-norm
+Fail=0
+
+# source ./run-yaml-testcases.sh $transducer
+
+for file in ${srcdir}/../../../src/morphology/*.lexc \
+			${srcdir}/../../../src/morphology/*/*.lexc; do
+	fsts=$(grep '^\!\!€[^ :]' $file | sed 's/.*€\([a-z-]*\)\:.*/\1/' | sort -u)
+	tests=$(grep '^\!\!€ ' $file)
+	if [ "$fsts" == "" -a "$tests" == "" ]; then
+		echo
+		echo "* WARNING: the LexC file $file"
+		echo "doesn't contain any tests - SKIPPED"
+		echo
+	elif [ "$fsts" == "" -a ! "$tests" == "" ]; then
+#		echo "$file has tests, but no fst specified - defaulting to gt-norm."
+#		source ./run-yaml-testcases.sh $transducer $file
+		echo
+		echo "$file has tests, but no fst specified - SKIPPED"
+		echo
+	else
+		for fst in $fsts; do
+		    (( i += 1 ))
+		    echo
+		    echo "LexC subtest $i: Testing $file using fst: $fst"
+		    echo
+			source ./run-morph-tester.sh $fst $file
+		done
+	fi
+done
+
+source error-handling-stubs.sh

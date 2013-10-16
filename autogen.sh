96d44a508ec14eb6c4e68def13685567f678e356
Sjur Moshagen <sjurnm@mac.com>
2013-10-16 16:50:46 +0000

    [Template merke - und:] Made the check for GTCORE functional, looking for both the gt-core.sh script (and using its output if found), and the environment variable $GTCORE. This means that there is no need anymore to set the GTCORE variable as long as one configure, make and make install in the gtcore directory.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fao@82206 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# The whole GT infra requires GTCORE to be accessible at setup time
-if test -z $GTCORE ; then
-    echo variable GTCORE not set, run gtsetup.sh and retry
-    exit 1
-fi
-
 echo "Initial automake setup of $(basename $(pwd))"
 
 # autoreconf should work for most platforms

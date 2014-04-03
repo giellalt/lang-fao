5af4d3743a92f69bde7832c35e631870c24c39f6
Sjur Moshagen <sjurnm@mac.com>
2014-04-03 11:30:12 +0000

    [Template merge - und] Moved yaml test scripts for different transducer types up one level, to correspond to the parallel location of the fst files in the build tree.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fao@91689 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ test/src/run-dict-gt-norm-genyaml-testcases.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This is a shell script that will call the actual test runner with the
+# specified transducer. This determines also the set of yaml test files looped
+# over by the test runner.
+
+###### Variables: #######
+transducer=dict-gt-norm
+halftest=gen # analysis test
+yaml_file_subdir=dict-gt-yamls
+
+# Find relative path from test script to test runner:
+relpath=.
+maintestrunner=run-morph-tester.sh
+helpertestrunner=run-yaml-testcases.sh
+
+# We use the maintestrunner to find the relative path, because it will be in
+# the path of the build tree - the helpertestrunner will only be in the path
+# of the source tree, and will not be found if using VPATH building.
+while test ! -x $relpath/$maintestrunner ; do
+    relpath="$relpath/.."                        # if not found, go one level up
+#   echo relpath: $relpath                       # debug
+    if test "$(cd $relpath && pwd)" = "/" ; then # have we reached the root?
+        echo "$0: No test runner found!"
+        exit 77
+    fi
+done
+
+testrunner="$srcdir/$relpath/$helpertestrunner"
+
+source $testrunner $transducer $yaml_file_subdir $halftest

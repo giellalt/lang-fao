2fe8263fc7779fa0c3f14ca62db5586082f5c750
Sjur N Moshagen <sjurnm@mac.com>
2015-03-13 10:24:25 +0000

    [Template merge - langs/und] Added a new directory named devtools/ to each language, with the idea that it should contain tools useful for development, but not necessarily suitable for automake testing. Initially it contains shell scripts to generate a table of generated word forms for each continuation lexicon.

---
+++ devtools/generate-adj-wordforms.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# A short shell script to test word form generation for all continuation
+# lexicons except the ones listed in the exception list.
+
+######### USER Variables - change these to your liking: #########
+# Codes for the word forms to be generated - list as many or few as needed:
+morf_codes="+A+Sg+Nom \
+            +A+Sg+Gen \
+            +A+Sg+Ill \
+            +A+Pl+Nom \
+            +A+Pl+Com \
+            +A+Attr"
+
+# Lexicon source file for lexicons and lemmas:
+source_file=src/morphology/stems/adjectives.lexc
+
+# Lexicons that should NOT be used to extract lemmas (egrep expression):
+exception_lexicons="(flagK)"
+
+# FST used for generation, MINUS suffix:
+generator_file=src/generator-gt-norm
+
+# How many lemmas maximally for each lexicon:
+lemmacount=10
+
+# Specify path to $GTCORE - we don't use Autotools for these scripts:
+gtcore=$GTCORE
+
+
+################## DO NOT CHANGE BELOW HERE!!! ##################
+source $gtcore/scripts/generate-wordforms-for-cont_lexes.sh \
+        "$gtcore" \
+        "$morf_codes" \
+        "$source_file" \
+        "$generator_file" \
+        "$lemmacount" \
+        "$exception_lexicons"

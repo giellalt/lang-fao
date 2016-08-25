cda9e74cf2209bcbe6b65093c42e79d39be79362
Sjur Moshagen <sjurnm@mac.com>
2016-08-25 18:59:30 +0000

    [Template merge - langs/und] Changed the path to the css for the xml speller test results in devtools.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fao@139237 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/test_voikkospell_suggestions.sh
+++ devtools/test_voikkospell_suggestions.sh
@@ -105,7 +105,7 @@ $GTCORE/scripts/speller-testres.pl \
 fgrep -v '<?xml version' "$speller_results" > "$speller_results.tmp"
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" > "$speller_results"
 echo "<?xml-stylesheet \
-href=\"https://gtsvn.uit.no/langtech/trunk/gtcore/scripts/style/speller_xml.css\" \
+href=\"https://gtsvn.uit.no/langtech/trunk/giella-core/scripts/style/speller_xml.css\" \
 type=\"text/css\"?>"   >> "$speller_results"
 cat "$speller_results.tmp" >> "$speller_results"
 rm -f "$speller_results.tmp"

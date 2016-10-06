568ca8254a94c4e6fbb5202c06c6ad33437ba607
Sjur N Moshagen <sjurnm@mac.com>
2016-10-06 15:28:05 +0000

    [Template merge - langs/und] More robustness added to the test scripts: checking several variables, testing whether the found variables are pointing to existing directories, and giving an error message if no directory is found.

--- devtools/generate-prop-wordforms.sh
+++ devtools/generate-prop-wordforms.sh
@@ -3,6 +3,18 @@
 # A short shell script to test word form generation for all continuation
 # lexicons except the ones listed in the exception list.
 
+# Path to $GIELLA_CORE - we don't use Autotools for these scripts:
+if test "x$GIELLA_CORE" != "x" -a -d "$GIELLA_CORE" ; then
+    giella_core=$GIELLA_CORE
+elif test "x$GTCORE" != "x" -a -d "$GTCORE" ; then
+    giella_core=$GTCORE
+elif test "x$GTHOME" != "x" -a -d "$GTHOME/giella-core" ; then
+    giella_core=$GTHOME/giella-core
+else
+    echo "ERROR: Neither of $$GIELLA_CORE, $$GTCORE or $$GTHOME defined."
+    exit 1
+fi
+
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
 morf_codes="+N+Prop+Sg+Nom \
@@ -26,7 +38,7 @@ generator_file=src/generator-gt-norm
 lemmacount=10
 
 # Specify path to the dir containing the script used for generation:
-script_dir=$GTCORE/scripts
+script_dir=$giella_core/scripts
 
 ################## DO NOT CHANGE BELOW HERE!!! ##################
 source $script_dir/generate-wordforms-for-cont_lexes.sh \

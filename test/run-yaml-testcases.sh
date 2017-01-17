563e61e13e70e8c848ecfcb74b9ec8861820909d
Sjur Moshagen <sjurnm@mac.com>
2017-01-17 15:52:04 +0000

    [Template merge - langs/und] Fixed regression introduced in the previous commit: one-sided tests where included when looking for test data, causing a subsequent python fail when no actual test data was found. Fixed by using a stricter file name pattern.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/fao@146790 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/run-yaml-testcases.sh
+++ test/run-yaml-testcases.sh
@@ -16,18 +16,20 @@
 # Wrong usage - short instruction:
 if ! test $# -ge 2 ; then
     echo
-    echo "Usage: $0 TRANSDUCERTYPE RELPATH YAMLSUBDIR [HALFTEST]"
+    echo "Usage: $0 TRANSDUCER YAMLSUBDIR [HALFTEST]"
     echo
-    echo "were TRANSDUCERTYPE is the type of transducer targeted"
-    echo "for testing: gt-norm, gt-desc, etc. It will loop"
-    echo "over all yaml tests for the specified TRANSDUCERTYPE."
-    echo "The  argument YAMLSUBDIR specifies the subdir containing"
-    echo "yaml test files - the default is ./ (ie the current dir)."
-    echo "This makes it possible to move all yaml files out of the"
-    echo "test dir, making for a much cleaner directory."
-    echo "The optional argument HALFTEST is one of the strings"
-    echo "'ana' or 'gen', indicating that the test runs should"
-    echo "only run the analyser or generator tests respectively."
+    echo "were:"
+    echo " TRANSDUCER is the type of transducer targeted"
+    echo "            for testing: gt-norm, gt-desc, etc. It will loop"
+    echo "            over all yaml tests for the specified TRANSDUCER."
+    echo " YAMLSUBDIR specifies the subdir containing yaml"
+    echo "            yaml test files - the default is ./ (ie the current"
+    echo "            dir). This makes it possible to move all yaml files"
+    echo "            out of the test dir, making for a much cleaner"
+    echo "            directory."
+    echo " [HALFTEST] (optional) is one of the strings 'ana' or 'gen',"
+    echo "            indicating that the test runs should only run"
+    echo "            the analyser or generator tests respectively."
     echo
     exit 77
 fi
@@ -82,19 +84,23 @@ fi
 
 # The "*_$transducer.*.$suffix" pattern is for fst-specific yaml files:
 testfiles=$(find $srcdir/$yaml_file_subdir -name "*_$transducer.$suffix" -or \
-                                           -name "*_$transducer.*.$suffix")
+                                           -name "*_$transducer.????.$suffix")
 if test "x$testfiles" == "x"; then
     echo
     echo \
     "** No YAML files matching: $srcdir/$yaml_file_subdir/*_$transducer.$suffix"
+    echo \
+    "** No YAML files matching: $srcdir/$yaml_file_subdir/*_$transducer.????.$suffix"
     exit 77
 fi
+
+testfiles=$(find $srcdir/$yaml_file_subdir -name "*_$transducer.$suffix" )
 
 # One empty line in the beginning:
 echo ""
 i=0
 # Loop over the available yaml files (all fst types), and run the tests:
-for file in ${srcdir}/$yaml_file_subdir/*_$transducer.$suffix; do
+for file in $testfiles; do
     (( i += 1 ))
     leadtext=$(echo "YAML test $i: ")
     source ./$relpath/run-morph-tester.sh \

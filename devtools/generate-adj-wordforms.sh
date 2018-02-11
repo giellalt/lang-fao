7886c64aa452423d1c7c3f6bc70fb317bd170100
Trond Trosterud <trond.trosterud@uit.no>
2018-02-11 14:26:43 +0000

    færøyske former

--- devtools/generate-adj-wordforms.sh
+++ devtools/generate-adj-wordforms.sh
@@ -17,12 +17,73 @@ fi
 
 ######### USER Variables - change these to your liking: #########
 # Codes for the word forms to be generated - list as many or few as needed:
-morf_codes="+A+Sg+Nom \
-            +A+Sg+Gen \
-            +A+Sg+Ill \
-            +A+Pl+Nom \
-            +A+Pl+Com \
-            +A+Attr"
+morf_codes="+A+Msc+Sg+Nom+Indef		   \
+            +A+Msc+Sg+Acc+Indef		   \
+            +A+Msc+Sg+Dat+Indef		   \
+            +A+Msc+Sg+Gen+Indef		   \
+            +A+Msc+Pl+Nom+Indef		   \
+            +A+Msc+Pl+Acc+Indef		   \
+            +A+Msc+Pl+Dat+Indef		   \
+            +A+Msc+Pl+Gen+Indef		   \
+            +A+Msc+Sg+Nom+Def		   \
+            +A+Msc+Sg+Acc+Def		   \
+            +A+Msc+Pl+Nom+Def		   \
+            +A+Msc+Pl+Acc+Def		   \
+            +A+Fem+Sg+Nom+Indef		   \
+            +A+Fem+Sg+Acc+Indef		   \
+            +A+Fem+Sg+Dat+Indef		   \
+            +A+Fem+Sg+Gen+Indef		   \
+            +A+Fem+Pl+Nom+Indef		   \
+            +A+Fem+Pl+Acc+Indef		   \
+            +A+Fem+Pl+Dat+Indef		   \
+            +A+Fem+Pl+Gen+Indef		   \
+            +A+Fem+Sg+Nom+Def		   \
+            +A+Fem+Sg+Acc+Def		   \
+            +A+Fem+Pl+Nom+Def		   \
+            +A+Fem+Pl+Acc+Def		   \
+            +A+Neu+Sg+Nom+Indef		   \
+            +A+Neu+Sg+Acc+Indef		   \
+            +A+Neu+Sg+Dat+Indef		   \
+            +A+Neu+Sg+Gen+Indef		   \
+            +A+Neu+Pl+Nom+Indef		   \
+            +A+Neu+Pl+Acc+Indef		   \
+            +A+Neu+Pl+Dat+Indef		   \
+            +A+Neu+Pl+Gen+Indef		   \
+            +A+Neu+Sg+Nom+Def		   \
+            +A+Neu+Sg+Acc+Def		   \
+            +A+Neu+Pl+Nom+Def		   \
+            +A+Neu+Pl+Acc+Def		   \
+            +A+Cmp+Msc+Sg+Nom+Indef	   \
+            +A+Cmp+Msc+Pl+Nom+Indef	   \
+            +A+Cmp+Msc+Sg+Nom+Def	   \
+            +A+Cmp+Msc+Pl+Nom+Def	   \
+            +A+Cmp+Fem+Sg+Nom+Indef	   \
+            +A+Cmp+Fem+Pl+Nom+Indef	   \
+            +A+Cmp+Fem+Sg+Nom+Def	   \
+            +A+Cmp+Fem+Pl+Nom+Def	   \
+            +A+Cmp+Neu+Sg+Nom+Indef	   \
+            +A+Cmp+Neu+Sg+Acc+Indef	   \
+            +A+Cmp+Neu+Pl+Nom+Indef	   \
+            +A+Cmp+Neu+Pl+Acc+Indef	   \
+            +A+Cmp+Neu+Sg+Nom+Def	   \
+            +A+Cmp+Neu+Sg+Acc+Def	   \
+            +A+Cmp+Neu+Pl+Nom+Def	   \
+            +A+Cmp+Neu+Pl+Acc+Def	   \
+            +A+Superl+Msc+Sg+Nom+Indef \
+            +A+Superl+Msc+Pl+Nom+Indef \
+            +A+Superl+Msc+Sg+Nom+Def   \
+            +A+Superl+Msc+Pl+Nom+Def   \
+            +A+Superl+Fem+Sg+Nom+Indef \
+            +A+Superl+Fem+Sg+Acc+Indef \
+            +A+Superl+Fem+Pl+Nom+Indef \
+            +A+Superl+Fem+Sg+Nom+Def   \
+            +A+Superl+Fem+Pl+Nom+Def   \
+            +A+Superl+Neu+Sg+Nom+Indef \
+            +A+Superl+Neu+Sg+Acc+Indef \
+            +A+Superl+Neu+Pl+Nom+Indef \
+            +A+Superl+Neu+Pl+Acc+Indef \
+            +A+Superl+Neu+Sg+Nom+Def   \
+            +A+Superl+Neu+Pl+Nom+Def"
 
 # Lexicon source file for lexicons and lemmas:
 source_file=src/morphology/stems/adjectives.lexc
@@ -34,7 +95,7 @@ exception_lexicons="(flagK)"
 generator_file=src/generator-gt-norm
 
 # How many lemmas maximally for each lexicon:
-lemmacount=10
+lemmacount=2
 
 # Specify path to the dir containing the script used for generation:
 script_dir=$giella_core/scripts

4e4ffdfd8f7d91628a320e11b62aed16d1084d29
Trond Trosterud <trond.trosterud@uit.no>
2020-06-14 21:36:28 +0200

    Fil for å sjekke om alle taggane er deklarert i root.lexc

---
+++ devtools/tag_test.sh
@@ -0,0 +1,12 @@
+#shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
+
+echo 'Are there tags not declared in root.lexc or misspelled?'
+cat src/fst/compounding.lexc  src/fst/affixes/*lexc src/fst/stems/*lexc  ../giella-shared/smi/src/fst/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+
+cat src/fst/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+
+echo 'Have a look at these:'
+
+comm -23 lexctags roottags 
+echo 'checked'
+rm lexctags roottags

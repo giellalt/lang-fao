giellalt/lang-est-x-utee

[Template merge - und] Encapsulated most shell variable names in {} to handle hyphens etc in the variable names (after merge/update substitution of the __UND__ string).

autogen.sh
@@ -60,13 +60,13 @@ if test x$langvar = xlangvar ; then
 		echo
 		exit 1
 	else
-		if test x$GTLANG_est = x$LANGDIR ; then
-			echo "\$GTLANG_est already defined."
+		if test x${GTLANG_est} = x${LANGDIR} ; then
+			echo "\${GTLANG_est} already defined."
 		else
 			# Already defined with a different value:
-			if test x$GTLANG_est != x ; then
+			if test x${GTLANG_est} != x ; then
 				renew=renew
-				OLDLANGDIR=$GTLANG_est
+				OLDLANGDIR=${GTLANG_est}
 			fi
 
 			# Add the variable to the login script:
@@ -76,14 +76,15 @@ if test x$langvar = xlangvar ; then
 
 			# Feedback depending on whether it was added or redefined:
 			if test x$renew = xrenew ; then
-			 echo "The env. variable \$GTLANG_est has been redefined in"
-			 echo "$LOGINFILE. The old value $OLDLANGDIR is no longer in use."
+			 echo "The env. variable \${GTLANG_est} has been redefined in"
+			 echo "$LOGINFILE. The old value ${OLDLANGDIR} is no longer in use."
 			else
-			 echo "The env. variable \$GTLANG_est has been added to $LOGINFILE."
-    		 echo "Your built fst's (those you get after 'make') will be used"
-    		 echo "with the analyser and generator scripts. There's a backup of"
-    		 echo "your old $LOGINFILE in $LOGINFILE.gtbackup.${DATE}-${TIME}."
-    		 echo "Please log out and log in again for the variable to be used."
+			 echo "The env. variable \${GTLANG_est} has been added"
+			 echo "to $LOGINFILE. Your built fst's (those you get after"
+			 echo "'make') will be used with the analyser and generator"
+			 echo "scripts. There's a backup of your old $LOGINFILE in"
+			 echo " $LOGINFILE.gtbackup.${DATE}-${TIME}."
+    		 echo "Please log out and in again for the variable to take effect."
 			fi
 		fi
 	fi
@@ -91,18 +92,19 @@ fi
 
 # Check whether the variable is defined, warn the user if not or different from
 # the current dir:
-if   test x$GTLANG_est = x ; then
-	echo "WARNING: The variable \$GTLANG_est has not been defined. You will not"
-	echo "be able to use your own fst's with the analyser and generator scripts"
-	echo "if not defined. Please consider rerunning this script with option -l:"
+if   test x${GTLANG_est} = x ; then
+	echo "WARNING: The variable \${GTLANG_est} has not been defined. You"
+	echo "will not be able to use your own fst's with the analyser and"
+	echo "generator scripts if not defined. Please consider rerunning this"
+	echo "script with option -l:"
 	echo
 	echo "$0 -l"
 	echo
-elif test x$GTLANG_est != x$LANGDIR ; then
-	echo "WARNING: The variable \$GTLANG_est has the value:"
-	echo "  $GTLANG_est"
+elif test x${GTLANG_est} != x${LANGDIR} ; then
+	echo "WARNING: The variable \${GTLANG_est} has the value:"
+	echo "  ${GTLANG_est}"
 	echo "instead of the expected:"
-	echo "  $LANGDIR"
+	echo "  ${LANGDIR}"
 	echo "Please consider rerunning this script with option -l to update the"
 	echo "variable:"
 	echo

giellalt/lang-fao

autogen.sh
@@ -60,13 +60,13 @@ if test x$langvar = xlangvar ; then
 		echo
 		exit 1
 	else
-		if test x$GTLANG_fao = x$LANGDIR ; then
-			echo "\$GTLANG_fao already defined."
+		if test x${GTLANG_fao} = x${LANGDIR} ; then
+			echo "\${GTLANG_fao} already defined."
 		else
 			# Already defined with a different value:
-			if test x$GTLANG_fao != x ; then
+			if test x${GTLANG_fao} != x ; then
 				renew=renew
-				OLDLANGDIR=$GTLANG_fao
+				OLDLANGDIR=${GTLANG_fao}
 			fi
 
 			# Add the variable to the login script:
@@ -76,14 +76,15 @@ if test x$langvar = xlangvar ; then
 
 			# Feedback depending on whether it was added or redefined:
 			if test x$renew = xrenew ; then
-			 echo "The env. variable \$GTLANG_fao has been redefined in"
-			 echo "$LOGINFILE. The old value $OLDLANGDIR is no longer in use."
+			 echo "The env. variable \${GTLANG_fao} has been redefined in"
+			 echo "$LOGINFILE. The old value ${OLDLANGDIR} is no longer in use."
 			else
-			 echo "The env. variable \$GTLANG_fao has been added to $LOGINFILE."
-    		 echo "Your built fst's (those you get after 'make') will be used"
-    		 echo "with the analyser and generator scripts. There's a backup of"
-    		 echo "your old $LOGINFILE in $LOGINFILE.gtbackup.${DATE}-${TIME}."
-    		 echo "Please log out and log in again for the variable to be used."
+			 echo "The env. variable \${GTLANG_fao} has been added"
+			 echo "to $LOGINFILE. Your built fst's (those you get after"
+			 echo "'make') will be used with the analyser and generator"
+			 echo "scripts. There's a backup of your old $LOGINFILE in"
+			 echo " $LOGINFILE.gtbackup.${DATE}-${TIME}."
+    		 echo "Please log out and in again for the variable to take effect."
 			fi
 		fi
 	fi
@@ -91,18 +92,19 @@ fi
 
 # Check whether the variable is defined, warn the user if not or different from
 # the current dir:
-if   test x$GTLANG_fao = x ; then
-	echo "WARNING: The variable \$GTLANG_fao has not been defined. You will not"
-	echo "be able to use your own fst's with the analyser and generator scripts"
-	echo "if not defined. Please consider rerunning this script with option -l:"
+if   test x${GTLANG_fao} = x ; then
+	echo "WARNING: The variable \${GTLANG_fao} has not been defined. You"
+	echo "will not be able to use your own fst's with the analyser and"
+	echo "generator scripts if not defined. Please consider rerunning this"
+	echo "script with option -l:"
 	echo
 	echo "$0 -l"
 	echo
-elif test x$GTLANG_fao != x$LANGDIR ; then
-	echo "WARNING: The variable \$GTLANG_fao has the value:"
-	echo "  $GTLANG_fao"
+elif test x${GTLANG_fao} != x${LANGDIR} ; then
+	echo "WARNING: The variable \${GTLANG_fao} has the value:"
+	echo "  ${GTLANG_fao}"
 	echo "instead of the expected:"
-	echo "  $LANGDIR"
+	echo "  ${LANGDIR}"
 	echo "Please consider rerunning this script with option -l to update the"
 	echo "variable:"
 	echo
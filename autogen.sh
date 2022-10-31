giellalt/lang-izh

[Template merge - und] Added option to automatically create a language home dir environment variable. The idea is that by setting this variable, we can reliably find transducers in the working copy dirs of the users. The default is to not do anything (but give a warning). As part of this change, I switched the shell from sh to bash, as I don't know how portable the extra code is with respect to other shells.

autogen.sh
@@ -1,6 +1,111 @@
-#!/bin/sh
+#!/bin/bash
 
+function print_usage() {
+    echo "Usage: $0 [OPTIONS...]"
+    echo "Prepare Autotools build infrastructure"
+    echo
+    echo "  -h, --help          print this usage info"
+    echo "  -l, --add-langvar   Add env. variable \$GTLANG_izh to login script."
+    echo "                      This makes it possible to use the built fst's"
+    echo "                      with the analyser and generator scripts."
+    echo
+}
+
+# option variables
+langvar=""
+
+# manual getopt loop... Mac OS X does not have good getopt
+while test $# -ge 1 ; do
+    if test x$1 = x--add-langvar -o x$1 = x-l ; then
+        langvar=langvar
+    elif test x$1 = x--help -o x$1 = x-h ; then
+        print_usage
+        exit 0
+    else
+        echo
+        echo "ERROR: $0: unknown option $1"
+        print_usage
+        exit 1
+    fi
+    shift
+done
+
+# Variable setup for adding env. variable:
+GTLANG=izh
+LANGDIR=$(pwd)
+TIME=$(date +%H%M)
+DATE=$(date +%Y%m%d)
+LOGINFILE=
+
+# Find the login file:
+if [[ -r ~/.bash_profile ]]; then
+	LOGINFILE=~/.bash_profile
+elif [[ -r ~/.profile ]]; then
+	LOGINFILE=~/.profile
+elif [[ -r ~/.bashrc ]]; then
+	LOGINFILE=~/.bashrc
+fi
+
+echo
 echo "Initial automake setup of $(basename $(pwd))"
 
 # autoreconf should work for most platforms
 autoreconf -i
+
+# If option -l / --add-langvar was used:
+if test x$langvar = xlangvar ; then
+	if test x$LOGINFILE = x ; then
+		echo
+		echo "ERROR: could not find a login script to add the variable to!"
+		echo
+		exit 1
+	else
+		if test x$GTLANG_izh = x$LANGDIR ; then
+			echo "\$GTLANG_izh already defined."
+		else
+			# Already defined with a different value:
+			if test x$GTLANG_izh != x ; then
+				renew=renew
+				OLDLANGDIR=$GTLANG_izh
+			fi
+
+			# Add the variable to the login script:
+			cp $LOGINFILE $LOGINFILE.gtbackup.${DATE}-${TIME}
+			echo "export GTLANG_izh=$LANGDIR" >> $LOGINFILE
+			source $LOGINFILE
+
+			# Feedback depending on whether it was added or redefined:
+			if test x$renew = xrenew ; then
+			 echo "The env. variable \$GTLANG_izh has been redefined in"
+			 echo "$LOGINFILE. The old value $OLDLANGDIR is no longer in use."
+			else
+			 echo "The env. variable \$GTLANG_izh has been added to $LOGINFILE."
+    		 echo "Your built fst's (those you get after 'make') will be used"
+    		 echo "with the analyser and generator scripts. There's a backup of"
+    		 echo "your old $LOGINFILE in $LOGINFILE.gtbackup.${DATE}-${TIME}."
+    		 echo "Please log out and log in again for the variable to be used."
+			fi
+		fi
+	fi
+fi
+
+# Check whether the variable is defined, warn the user if not or different from
+# the current dir:
+if   test x$GTLANG_izh = x ; then
+	echo "WARNING: The variable \$GTLANG_izh has not been defined. You will not"
+	echo "be able to use your own fst's with the analyser and generator scripts"
+	echo "if not defined. Please consider rerunning this script with option -l:"
+	echo
+	echo "$0 -l"
+	echo
+elif test x$GTLANG_izh != x$LANGDIR ; then
+	echo "WARNING: The variable \$GTLANG_izh has the value:"
+	echo "  $GTLANG_izh"
+	echo "instead of the expected:"
+	echo "  $LANGDIR"
+	echo "Please consider rerunning this script with option -l to update the"
+	echo "variable:"
+	echo
+	echo "$0 -l"
+	echo
+fi

giellalt/lang-fao

autogen.sh
@@ -1,6 +1,111 @@
-#!/bin/sh
+#!/bin/bash
 
+function print_usage() {
+    echo "Usage: $0 [OPTIONS...]"
+    echo "Prepare Autotools build infrastructure"
+    echo
+    echo "  -h, --help          print this usage info"
+    echo "  -l, --add-langvar   Add env. variable \$GTLANG_fao to login script."
+    echo "                      This makes it possible to use the built fst's"
+    echo "                      with the analyser and generator scripts."
+    echo
+}
+
+# option variables
+langvar=""
+
+# manual getopt loop... Mac OS X does not have good getopt
+while test $# -ge 1 ; do
+    if test x$1 = x--add-langvar -o x$1 = x-l ; then
+        langvar=langvar
+    elif test x$1 = x--help -o x$1 = x-h ; then
+        print_usage
+        exit 0
+    else
+        echo
+        echo "ERROR: $0: unknown option $1"
+        print_usage
+        exit 1
+    fi
+    shift
+done
+
+# Variable setup for adding env. variable:
+GTLANG=fao
+LANGDIR=$(pwd)
+TIME=$(date +%H%M)
+DATE=$(date +%Y%m%d)
+LOGINFILE=
+
+# Find the login file:
+if [[ -r ~/.bash_profile ]]; then
+	LOGINFILE=~/.bash_profile
+elif [[ -r ~/.profile ]]; then
+	LOGINFILE=~/.profile
+elif [[ -r ~/.bashrc ]]; then
+	LOGINFILE=~/.bashrc
+fi
+
+echo
 echo "Initial automake setup of $(basename $(pwd))"
 
 # autoreconf should work for most platforms
 autoreconf -i
+
+# If option -l / --add-langvar was used:
+if test x$langvar = xlangvar ; then
+	if test x$LOGINFILE = x ; then
+		echo
+		echo "ERROR: could not find a login script to add the variable to!"
+		echo
+		exit 1
+	else
+		if test x$GTLANG_fao = x$LANGDIR ; then
+			echo "\$GTLANG_fao already defined."
+		else
+			# Already defined with a different value:
+			if test x$GTLANG_fao != x ; then
+				renew=renew
+				OLDLANGDIR=$GTLANG_fao
+			fi
+
+			# Add the variable to the login script:
+			cp $LOGINFILE $LOGINFILE.gtbackup.${DATE}-${TIME}
+			echo "export GTLANG_fao=$LANGDIR" >> $LOGINFILE
+			source $LOGINFILE
+
+			# Feedback depending on whether it was added or redefined:
+			if test x$renew = xrenew ; then
+			 echo "The env. variable \$GTLANG_fao has been redefined in"
+			 echo "$LOGINFILE. The old value $OLDLANGDIR is no longer in use."
+			else
+			 echo "The env. variable \$GTLANG_fao has been added to $LOGINFILE."
+    		 echo "Your built fst's (those you get after 'make') will be used"
+    		 echo "with the analyser and generator scripts. There's a backup of"
+    		 echo "your old $LOGINFILE in $LOGINFILE.gtbackup.${DATE}-${TIME}."
+    		 echo "Please log out and log in again for the variable to be used."
+			fi
+		fi
+	fi
+fi
+
+# Check whether the variable is defined, warn the user if not or different from
+# the current dir:
+if   test x$GTLANG_fao = x ; then
+	echo "WARNING: The variable \$GTLANG_fao has not been defined. You will not"
+	echo "be able to use your own fst's with the analyser and generator scripts"
+	echo "if not defined. Please consider rerunning this script with option -l:"
+	echo
+	echo "$0 -l"
+	echo
+elif test x$GTLANG_fao != x$LANGDIR ; then
+	echo "WARNING: The variable \$GTLANG_fao has the value:"
+	echo "  $GTLANG_fao"
+	echo "instead of the expected:"
+	echo "  $LANGDIR"
+	echo "Please consider rerunning this script with option -l to update the"
+	echo "variable:"
+	echo
+	echo "$0 -l"
+	echo
+fi
giellalt/lang-fin

[Template merge - langs/und] Corrected placeholder string for two-letter ISO language code.

devtools/test_ospell-office_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=se
+GTLANG2=fi
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

devtools/test_voikkospell_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=fin
+GTLANG2=fi
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

giellalt/lang-fao

devtools/test_ospell-office_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=se
+GTLANG2=fo
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"

devtools/test_voikkospell_suggestions.sh
@@ -8,7 +8,7 @@
 # set -x
 
 # Language being tested, ISO 639-1 code if available:
-GTLANG2=fao
+GTLANG2=fo
 
 # Directory variables:
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
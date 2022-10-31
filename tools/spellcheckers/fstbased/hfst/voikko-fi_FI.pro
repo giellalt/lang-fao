giellalt/lang-est-x-plamk

Reorganised the tools/ dir to fit better with coming development. Template update.

tools/spellcheckers/fstbased/hfst/voikko-fi_FI.pro
@@ -1,0 +1,8 @@
+info: Voikko-Dictionary-Format: 2
+info: Language-Code: et
+info: Language-Variant: hfst
+info: Description: Spell-checker for Estonian generated from giellatekno sources for HFST
+info: Morphology-Backend: hfst
+info: Speller-Backend: hfst
+info: Suggestion-Backend: hfst
+info: Hyphenator-Backend: hfst

giellalt/lang-fao

tools/spellcheckers/fstbased/hfst/voikko-fi_FI.pro
@@ -1,0 +1,8 @@
+info: Voikko-Dictionary-Format: 2
+info: Language-Code: fo
+info: Language-Variant: hfst
+info: Description: Spell-checker for Faroese generated from giellatekno sources for HFST
+info: Morphology-Backend: hfst
+info: Speller-Backend: hfst
+info: Suggestion-Backend: hfst
+info: Hyphenator-Backend: hfst
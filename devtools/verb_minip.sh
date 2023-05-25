277a04ca98ec2c0a1a86938fc496f08ab539d4c8
fo-raettstavari <hedinj@setur.fo>
2023-05-25 15:49:08 +0100

    Add ref to short verbfile and desc analyser

--- devtools/verb_minip.sh
+++ devtools/verb_minip.sh
@@ -14,13 +14,15 @@ PATTERN=$1
 L_FILE="in.txt"
 cut -d '!' -f1 src/fst/stems/verbs.lexc | egrep $PATTERN | cut -d ':' -f1>$L_FILE
 
-P_FILE="test/data/testverbparadigm.txt"
+#P_FILE="test/data/testverbparadigm.txt"
+P_FILE="test/data/testverbparadigm_kort.txt"
 
 for lemma in $(cat $L_FILE);
 do
  for form in $(cat $P_FILE);
  do
    echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-fao/src/generator-gt-norm.hfstol
+#   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-fao/src/generator-gt-desc.hfstol
  done
  rm -f $L_FILE
 done

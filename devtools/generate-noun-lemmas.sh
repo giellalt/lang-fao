8e1fac50bcaf10d43ab311f96ec88f9ba74c41d5
Flammie A Pirinen <flammie@iki.fi>
2025-05-19 22:53:58 +0200

    [Template merge] lemma gen devtools

---
+++ devtools/generate-noun-lemmas.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if test -z "$GIELLA_CORE" ; then
+    echo "environment variable GIELLA_CORE needs to be set up"
+    exit 1
+elif ! test -d "$GIELLA_CORE" ; then
+    echo "GIELLA_CORE directory: $GIELLA_CORE may not be correct..."
+    exit 1
+elif ! test -x src/fst/morphology/test/generate-noun-lemmas.sh ; then
+    echo "test src/fst/morphology/test/generate-noun-lemmas.sh not found"
+    echo "(you should run this from lang-XXX directory)"
+    exit 1
+fi
+if which see ; then
+    logger="see"
+elif test -n "$EDITOR" ; then
+    logger=$EDITOR
+else
+    logger="cat"
+fi
+tmplog=$(mktemp -t giella-generate-test.XXXXXXXXXXX)
+
+srcdir=. src/fst/morphology/test/generate-noun-lemmas.sh > "$tmplog"
+if grep -F FAIL < "$tmplog" ; then
+    logfile=$(grep -E '^see ' < "$tmplog" | cut -d ' ' -f 2)
+    echo "running $logger $logfile"
+    if test "$logger" == "see" ; then
+        "$logger" "$logfile" &
+    else
+        "$logger" "$logfile"
+    fi
+fi

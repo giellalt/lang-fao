7d298f93847498bb2ccbd6282f14330ef4eb27a2
Sjur N Moshagen <sjurnm@mac.com>
2025-10-13 19:19:15 +0300

    [Template merge] Update drb template to follow the grammar checker template setup, add labels to steps, add dev mode pipeline

--- tools/grammarcheckers/pipeline.ts
+++ tools/grammarcheckers/pipeline.ts
@@ -4,21 +4,18 @@ import * as hfst from './.divvun-rt/hfst.ts';
 import { Command, StringEntry } from './.divvun-rt/mod.ts';
 
 export default function smeGramRelease(entry: StringEntry): Command {
-    let x = hfst.tokenize(entry, { model_path: "tokeniser-gramcheck-gt-desc.pmhfst" });
-    x = divvun.blanktag(x, { model_path: "analyser-gt-whitespace.hfst" });
-    x = cg3.vislcg3(x, { model_path: "mwe-dis.bin" });
-    x = cg3.mwesplit(x);
-    x = divvun.blanktag(x, { model_path: "analyser-gt-whitespace.hfst" });
-    x = divvun.cgspell(x, {
+  let x = hfst.tokenize("tokenize", entry, { model_path: "tokeniser-gramcheck-gt-desc.pmhfst" });
+  x = divvun.blanktag("whitespace", x, { model_path: "analyser-gt-whitespace.hfst" });
+  x = cg3.vislcg3("mwe-dis", x, { model_path: "mwe-dis.bin" });
+  x = cg3.mwesplit("mwesplit", x);
+  x = divvun.blanktag("whitespace", x, { model_path: "analyser-gt-whitespace.hfst" });
+  x = divvun.cgspell("speller", x, {
         acc_model_path: "acceptor.default.hfst",
         err_model_path: "errmodel.default.hfst"
     });
-    x = cg3.vislcg3(x, { model_path: "grc-disambiguator.bin" });
-    x = cg3.vislcg3(x, { model_path: "spellchecker.bin" });
-    x = cg3.vislcg3(x, { model_path: "after-speller-disambiguator.bin" });
-    x = cg3.vislcg3(x, { model_path: "grammarchecker.bin" });
-    x = divvun.suggest("suggest", x, {
-        model_path: "generator-gramcheck-gt-norm.hfstol"
-    });
-    return x;
+  x = cg3.vislcg3("grc-disamb", x, { model_path: "grc-disambiguator.bin" });
+  x = cg3.vislcg3("spell-sugg-filtering", x, { model_path: "spellchecker.bin" });
+  x = cg3.vislcg3("after-spell-disamb", x, { model_path: "after-speller-disambiguator.bin" });
+  x = cg3.vislcg3("gramcheck", x, { model_path: "grammarchecker.bin" });
+  return divvun.suggest("suggestions", x, { model_path: "generator-gramcheck-gt-norm.hfstol" });
 }

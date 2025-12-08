76c7f031dfbed52b1b5011e37166abb92bbea1b0
Sjur N Moshagen <sjurnm@mac.com>
2025-12-08 17:28:40 +0200

    [Template merge] Add pipeline with tracing to grammar checker

--- tools/grammarcheckers/pipeline.ts
+++ tools/grammarcheckers/pipeline.ts
@@ -19,3 +19,22 @@ export default function smeGramRelease(entry: StringEntry): Command {
   x = cg3.vislcg3("gramcheck", x, { model_path: "grammarchecker.bin" });
   return divvun.suggest("suggestions", x, { model_path: "generator-gramcheck-gt-norm.hfstol" });
 }
+
+/**
+ * Dev pipeline for testing with local models, with tracing.
+ */
+export function localTestTrace_dev(entry: StringEntry): Command {
+  let x = hfst.tokenize("tokenize", entry, { model_path: "@./tokeniser-gramcheck-gt-desc.pmhfst" });
+  x = divvun.blanktag("whitespace", x, { model_path: "@./analyser-gt-whitespace.hfst" });
+  x = cg3.vislcg3("mwe-dis", x, { model_path: "@../tokenisers/mwe-dis.cg3", config: { trace: true } });
+  x = cg3.mwesplit("mwesplit", x);
+  x = divvun.blanktag("errorwhitespace", x, { model_path: "@./analyser-gt-errorwhitespace.hfst" });
+  x = divvun.cgspell("speller", x, {
+    acc_model_path: "@./acceptor.default.hfst",
+    err_model_path: "@./errmodel.default.hfst",
+  });
+  x = cg3.vislcg3("disamb", x, { model_path: "@../../src/cg3/disambiguator.cg3", config: { trace: true } });
+  x = cg3.vislcg3("spell-sugg-filtering", x, { model_path: "@./spellchecker.cg3", config: { trace: true } });
+  x = cg3.vislcg3("gramcheck", x, { model_path: "@./grammarchecker.cg3", config: { trace: true } });
+  return divvun.suggest("suggestions", x, { model_path: "@./generator-gramcheck-gt-norm.hfstol" });
+}
